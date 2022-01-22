dcc64a276443971e3262fa233d70809f0e808334
Olivier Georgeon <olivier.georgeon@gmail.com>
2022-01-22 18:28:39 +0100

    Cleanup Controller.py

--- Controller.py
+++ Controller.py
@@ -5,6 +5,8 @@ from WifiInterface import WifiInterface
 from Phenomenon import Phenomenon
 import math
 from OsoyooCar import OsoyooCar
+from EgoMemoryWindow import EgoMemoryWindow
+import pyglet
 
 
 class Controller:
@@ -17,71 +19,90 @@ class Controller:
         self.phenomena = []
         self.robot = OsoyooCar(self.view.batch)
 
-        self.async_action = ""
-        self.async_flag = 0
-        self.async_outcome_string = ""
+        self.action = ""
+        self.enact_step = 0
+        self.outcome_string = ""
 
-    def process_outcome(self, text, outcome_string):
-        outcome = json.loads(outcome_string)
-        # floor_outcome = outcome['outcome']  # Agent5 uses floor_outcome
+    def watch_outcome(self, dt):
+        if self.enact_step == 2:
+            print(self.outcome_string)
+            outcome = json.loads(self.outcome_string)
+            # floor_outcome = outcome['outcome']  # Agent5 uses floor_outcome
 
-        # Presupposed displacement of the robot relative to the environment
-        translation = [0, 0]
-        rotation = 0
-        if text == "1":
-            rotation = 45
-        if text == "2":
-            translation[0] = -STEP_FORWARD_DISTANCE
-        if text == "3":
-            rotation = -45
-        if text == "8":
-            translation[0] = STEP_FORWARD_DISTANCE
+            # Presupposed displacement of the robot relative to the environment
+            translation = [0, 0]
+            rotation = 0
+            if self.action == "1":
+                rotation = 45
+            if self.action == "2":
+                translation[0] = -STEP_FORWARD_DISTANCE
+            if self.action == "3":
+                rotation = -45
+            if self.action == "8":
+                translation[0] = STEP_FORWARD_DISTANCE
 
-        # Actual measured displacement if any
-        if 'yaw' in outcome:
-            rotation = outcome['yaw']
+            # Actual measured displacement if any
+            if 'yaw' in outcome:
+                rotation = outcome['yaw']
 
-        # Estimate displacement due to floor change retreat
-        if 'floor' in outcome:
-            if outcome['floor'] > 0:  # Black line detected
-                # Update the translation
-                if text == "8":  # TODO Other actions
-                    forward_duration = outcome['duration'] - 300  # Subtract retreat duration
-                    translation[0] = STEP_FORWARD_DISTANCE * forward_duration/1000 - RETREAT_DISTANCE  # To be adjusted
-                # Create a new floor-changed phenomenon
-                obstacle = Phenomenon(150 + translation[0], 0, self.view.batch, 1)  # the translation will be reapplied
-                self.phenomena.append(obstacle)
+            # Estimate displacement due to floor change retreat
+            if 'floor' in outcome:
+                if outcome['floor'] > 0:  # Black line detected
+                    # Update the translation
+                    if self.action == "8":  # TODO Other actions
+                        forward_duration = outcome['duration'] - 300  # Subtract retreat duration
+                        translation[0] = STEP_FORWARD_DISTANCE * forward_duration/1000 - RETREAT_DISTANCE  # To be adjusted
+                    # Create a new floor-changed phenomenon
+                    obstacle = Phenomenon(150 + translation[0], 0, self.view.batch, 1)  # the translation will be reapplied
+                    self.phenomena.append(obstacle)
 
-        # Translate and rotate all the phenomena
-        for p in self.phenomena:
-            p.translate(translation)
-            p.rotate(rotation)
+            # Translate and rotate all the phenomena
+            for p in self.phenomena:
+                p.translate(translation)
+                p.rotate(rotation)
 
-        # Update head angle
-        if 'head_angle' in outcome:
-            head_angle = outcome['head_angle']
-            self.robot.rotate_head(head_angle)
-            if text == "-" or text == "*" or text == "1" or text == "3":
-                # Create a new echo phenomenon
-                echo_distance = outcome['echo_distance']
-                print("Echo distance %i" % echo_distance)
-                x = self.robot.head_x + math.cos(math.radians(head_angle)) * echo_distance
-                y = self.robot.head_y + math.sin(math.radians(head_angle)) * echo_distance
-                obstacle = Phenomenon(x, y, self.view.batch)
-                self.phenomena.append(obstacle)
-        self.view.update_environment_matrix(translation, rotation)
+            # Update head angle
+            if 'head_angle' in outcome:
+                head_angle = outcome['head_angle']
+                self.robot.rotate_head(head_angle)
+                if self.action == "-" or self.action == "*" or self.action == "1" or self.action == "3":
+                    # Create a new echo phenomenon
+                    echo_distance = outcome['echo_distance']
+                    print("Echo distance %i" % echo_distance)
+                    x = self.robot.head_x + math.cos(math.radians(head_angle)) * echo_distance
+                    y = self.robot.head_y + math.sin(math.radians(head_angle)) * echo_distance
+                    obstacle = Phenomenon(x, y, self.view.batch)
+                    self.phenomena.append(obstacle)
+            self.enact_step = 0
+            self.view.update_environment_matrix(translation, rotation)
 
     # Asynchronous interaction with the robot
-    def async_action_trigger(self, text):
-        def async_action(controller: Controller):
-            print("Send " + self.async_action)
-            controller.async_outcome_string = controller.wifiInterface.enact(self.async_action)
-            print("Receive ", end="")
-            print(controller.async_outcome_string)
-            controller.async_flag = 2
+    def enact(self, text):
+        def enact_thread():
+            print("Send " + self.action)
+            self.outcome_string = self.wifiInterface.enact(self.action)
+            print("Receive " + self.outcome_string)
+            self.enact_step = 2
 
-        self.async_action = text
-        self.async_flag = 1
-        thread = threading.Thread(target=async_action, args=[self])
+        self.action = text
+        self.enact_step = 1
+        thread = threading.Thread(target=enact_thread)
         thread.start()
 
+
+if __name__ == "__main__":
+    emw = EgoMemoryWindow()
+    controller = Controller(emw)
+
+    @emw.event
+    def on_text(text):
+        if controller.enact_step == 0:
+            if text == "/":  # Turn of the angle marked by the mouse click
+                angle = int(math.degrees(math.atan2(emw.mouse_press_y, emw.mouse_press_x)))
+                text = json.dumps({'action': '/', 'angle': angle})
+            controller.enact(text)
+        else:
+            print("Waiting for previous outcome before sending new action")
+
+    pyglet.clock.schedule_interval(controller.watch_outcome, 0.1)
+    pyglet.app.run()
--- main.py
+++ main.py
@@ -4,29 +4,27 @@ from EgoMemoryWindow import EgoMemoryWindow
 import json
 import math
 
-view = EgoMemoryWindow()
-controller = Controller(view)
-
-
-@view.event
-def on_text(text):
-    if controller.async_flag == 0:
-        if text == "/":  # Turn of the angle marked by the mouse click
-            angle = int(math.degrees(math.atan2(view.mouse_press_y, view.mouse_press_x)))
-            text = json.dumps({'action': '/', 'angle': angle})
-        controller.async_action_trigger(text)
-    else:
-        print("Waiting for previous outcome before sending new action")
-
-
-def watch_async_outcome(dt):
-    if controller.async_flag == 2:
-        print("Redraw window")
-        controller.process_outcome(controller.async_action, controller.async_outcome_string)
-        controller.async_flag = 0
-
 
 def main():
+    view = EgoMemoryWindow()
+    controller = Controller(view)
+
+    @view.event
+    def on_text(text):
+        if controller.enact_step == 0:
+            if text == "/":  # Turn of the angle marked by the mouse click
+                angle = int(math.degrees(math.atan2(view.mouse_press_y, view.mouse_press_x)))
+                text = json.dumps({'action': '/', 'angle': angle})
+            controller.async_action_trigger(text)
+        else:
+            print("Waiting for previous outcome before sending new action")
+
+    def watch_async_outcome(dt):
+        if controller.enact_step == 2:
+            print("Redraw window")
+            controller.process_outcome(controller.action, controller.async_outcome_string)
+            controller.enact_step = 0
+
     pyglet.clock.schedule_interval(watch_async_outcome, 0.1)
     pyglet.app.run()
 
