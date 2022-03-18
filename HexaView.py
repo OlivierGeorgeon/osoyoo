ae4af9524a02a614bef53fcec524a792596bbfa7
Olivier Georgeon <olivier.georgeon@gmail.com>
2022-03-18 09:52:25 +0100

    HexaView zoomable

--- HexaView.py
+++ HexaView.py
@@ -5,16 +5,20 @@ import math
 import time
 from Utils import hexaMemory_to_pyglet
 ZOOM_IN_FACTOR = 1.2
+from HexaMemory import HexaMemory
 
+NB_CELL_WIDTH = 30
+NB_CELL_HEIGHT = 100
+CELL_RADIUS = 50
 
 class HexaView(pyglet.window.Window):
-    def __init__(self, width=600, height=400, shapesList = None, *args, **kwargs):
+    def __init__(self, width=400, height=400, shapesList = None, *args, **kwargs):
         super().__init__(width, height, resizable=True, *args, **kwargs)
         self.set_caption("Hexa Memory")
         self.set_minimum_size(150, 150)
         glClearColor(1.0, 1.0, 1.0, 1.0)
         self.batch = pyglet.graphics.Batch()
-        self.zoom_level = 10
+        self.zoom_level = 1
         self.azimuth = 0
         self.shapesList = shapesList
         # self.mouse_press_x = 0
@@ -52,7 +56,14 @@ class HexaView(pyglet.window.Window):
         # Stack the projection matrix. Centered on (0,0). Fit the window size and zoom factor  # DEBUG TODO ALED decentrer le bordel, ou plutot le centrer sur le milieu de la grid
         # glOrtho(1 * self.zoom_level, self.width * self.zoom_level , 1* self.zoom_level,
         #         self.height * self.zoom_level, 1, -1)
-        glOrtho(-50, 36*50 , -50, 36*50, 1, -1)
+        grid_width = NB_CELL_WIDTH * CELL_RADIUS * 3.3 # 36*50
+        grid_height = NB_CELL_HEIGHT * CELL_RADIUS  # 5000 36*50
+        left = -CELL_RADIUS  # -grid_width / 2  * self.zoom_level # * self.width
+        right = grid_width * self.zoom_level  # * self.width
+        bottom = -CELL_RADIUS # -center_y  * self.zoom_level  # * self.height
+        top = grid_height * self.zoom_level  # * self.height
+
+        glOrtho(left, right, bottom, top, 1, 0)
         # Draw the robot and the phenomena
         shapesListo = self.shapesList
         self.batch.draw()
@@ -71,3 +82,12 @@ class HexaView(pyglet.window.Window):
 
         def set_batch(self, batch):
             self.batch = batch
+
+
+# Testing by displaying HexaView with HexaMemory
+if __name__ == "__main__":
+    hexaview = HexaView()
+    hexa_memory = HexaMemory(width=NB_CELL_WIDTH, height=NB_CELL_HEIGHT, cells_radius=CELL_RADIUS)
+    hexaview.extract_and_convert_phenomenons(hexa_memory)
+
+    pyglet.app.run()
--- OsoyooCar.py
+++ OsoyooCar.py
@@ -1,8 +1,9 @@
 from pyglet import shapes
 from webcolors import name_to_rgb
 
-ROBOT_BODY_COLOR = name_to_rgb("darkblue")
+ROBOT_BODY_COLOR = name_to_rgb("darkslateblue")
 ROBOT_HEAD_COLOR = name_to_rgb("lightsteelblue")
+ROBOT_WHEEL_COLOR = name_to_rgb("darkblue")
 
 
 class OsoyooCar:
@@ -12,13 +13,13 @@ class OsoyooCar:
         # Create the body along the X axis (horizontal)
         self.robotBody = shapes.Rectangle(0, 0, 200, 160, color=ROBOT_BODY_COLOR, batch=self.batch)
         self.robotBody.anchor_position = 100, 80
-        self.FLWheel = shapes.Rectangle(50, 100, 80, 36, color=ROBOT_BODY_COLOR, batch=self.batch)
+        self.FLWheel = shapes.Rectangle(50, 100, 80, 36, color=ROBOT_WHEEL_COLOR, batch=self.batch)
         self.FLWheel.anchor_position = 40, 18
-        self.FRWheel = shapes.Rectangle(50, -100, 80, 36, color=ROBOT_BODY_COLOR, batch=self.batch)
+        self.FRWheel = shapes.Rectangle(50, -100, 80, 36, color=ROBOT_WHEEL_COLOR, batch=self.batch)
         self.FRWheel.anchor_position = 40, 18
-        self.RLWheel = shapes.Rectangle(-50, 100, 80, 36, color=ROBOT_BODY_COLOR, batch=self.batch)
+        self.RLWheel = shapes.Rectangle(-50, 100, 80, 36, color=ROBOT_WHEEL_COLOR, batch=self.batch)
         self.RLWheel.anchor_position = 40, 18
-        self.RRWheel = shapes.Rectangle(-50, -100, 80, 36, color=ROBOT_BODY_COLOR, batch=self.batch)
+        self.RRWheel = shapes.Rectangle(-50, -100, 80, 36, color=ROBOT_WHEEL_COLOR, batch=self.batch)
         self.RRWheel.anchor_position = 40, 18
 
         # Create the head
