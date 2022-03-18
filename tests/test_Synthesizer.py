205209a998b29042ddf28516009b0002f2959f9d
Olivier Georgeon <olivier.georgeon@gmail.com>
2022-03-18 15:26:05 +0100

    test_Synthesizer.py updated by Olivier

--- tests/test_Synthesizer.py
+++ tests/test_Synthesizer.py
@@ -1,23 +1,30 @@
-import sys
-import os
-import time
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+import pyglet
 from Synthesizer import Synthesizer
-from Interaction import Interaction
 from MemoryV1 import MemoryV1
 from HexaMemory import HexaMemory
 from HexaView import HexaView
 
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+
+# Testing the synthesiser
 if __name__ == '__main__':
+    # Initialize memory
     mem = MemoryV1()
-    hexaMemory = HexaMemory(20,20,cells_radius = 20)
-    synthe = Synthesizer(mem, hexaMemory)
-    view = HexaView()
-    it = Interaction(20,20)
-    mem.phenomenons.append(Interaction(200,200).red_line(41,21))
-    mem.phenomenons.append(Interaction(0,0).green_circle(100,100))
-    view.refresh(hexaMemory)
-    time.sleep(0.2)
+    # Add interactions to memory
+    mem.add((3, 0, 0, 0, 0, 0))  # Line
+    mem.add((0, 0, 0, 1, 300, -300))  # Echo
+
+    hexa_memory = HexaMemory(20, 40, cells_radius=50)
+    synthe = Synthesizer(mem, hexa_memory)
+
+    # Project egocentric memory to hexa memory
     synthe.act()
-    view.refresh(hexaMemory)
-    time.sleep(5)
+
+    # Create and populate hexa view
+    hexaview = HexaView()
+    hexaview.extract_and_convert_interactions(hexa_memory)
+
+    pyglet.app.run()
