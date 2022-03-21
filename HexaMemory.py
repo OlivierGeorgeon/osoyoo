6b79297de9e2c1c1eee19f8aede0d09f20ee1159
KNOCKAERT Titouan <tknockaert@univ-catholyon.fr>
2022-03-21 16:53:27 +0100

    Changement de systeme calcul coordonees, a l'air de mieux marcher

--- HexaMemory.py
+++ HexaMemory.py
@@ -46,7 +46,7 @@ class HexaMemory(HexaGrid):
             return
         self.orientation = 2
 
-    def convert_pos_in_cell(self, pos_x, pos_y):
+    def depr_convert_pos_in_cell(self, pos_x, pos_y):
         """blabla"""
         radius = self.cells_radius
         mini_radius = math.sqrt(radius**2 - (radius/2)**2)
@@ -145,6 +145,155 @@ class HexaMemory(HexaGrid):
         end_y = start_cell_y + y_decal + y_add
 
         return end_x, end_y
+
+
+    def convert_pos_in_cell(self,pos_x, pos_y):
+        """j'essaie autre chose"""
+        radius = self.cells_radius
+        mini_radius = math.sqrt(radius**2 - (radius/2)**2)
+        tmp_cell_x = self.width // 2
+        tmp_cell_y = self.height // 2
+        tmp_cell_x_center = 0
+        tmp_cell_y_center = 0
+        # Do the regular part of translation :
+        # to go to the next cell on the right/left you move by 3*radius on the x axis.
+        x_sign = 1
+        if pos_x < 0 :
+            x_sign = -1
+        y_sign = 1
+        if pos_y < 0 :
+            y_sign = -1
+        while abs(pos_x) >= abs(3*radius) :
+            tmp_cell_x += x_sign
+            pos_x -= (3*radius) * x_sign
+            tmp_cell_x_center += (3*radius) * x_sign
+        #To move to the cell on the top/bottom you move by 2*mini_radius on the y axis. 
+        while abs(pos_y) >= abs(2*mini_radius) :
+            tmp_cell_y += 2*y_sign
+            pos_y -= 2*mini_radius*y_sign
+            tmp_cell_y_center += 2*mini_radius*y_sign
+        # Elimination pour trouver dans quel voisin de la cellule courante on est
+        distance = math.sqrt(pos_x**2 + pos_y**2)
+        if(distance <= mini_radius): # On est forcement dans la bonne pos
+            return tmp_cell_x, tmp_cell_y
+        if distance <= radius :
+            "determiner la ligne qui risque d'etre traversée, et si on a passé la ligne ou non"
+        if abs(pos_x) >= 2.5 * radius:
+            "on est forcément dans g hgg bgg ou d hdd bdd"
+            if abs(pos_y) >= mini_radius :
+                "on est forcement dans hgg bgg hdd bdd"
+                return tmp_cell_x+ x_sign, tmp_cell_y + 2*y_sign
+            else :
+                "on est dans g ou d"
+                return tmp_cell_x + x_sign, tmp_cell_y
+
+        if abs(pos_x) >= 2 * radius :
+            "on est dans hgg g bgg hg bg / hdd d bdd hd bd "
+            if abs(pos_y) >= mini_radius :
+                "on est dans hgg-hg  bgg-bg  hdd-hd bd-bdd"
+                # On trouve l'équation de la ligne de démarcation
+                x_depart = 2.5 * radius
+                y_depart = mini_radius
+
+                x_fin = 2* radius
+                y_fin = 2*mini_radius
+
+                slope = (y_fin - y_depart) / (x_fin - x_depart)
+                offset = y_depart - (slope * x_depart)
+
+                y_ref = abs(pos_x) * slope + offset
+
+                if abs(pos_y) <= abs(y_ref) :
+                   "on est dans hg hd bg bd"
+                   return self.find_coordinates_corner(tmp_cell_x,tmp_cell_y, x_sign,y_sign)
+                else :
+                   "on est dans hgg bgg hdd bdd"
+                   return tmp_cell_x+ x_sign, tmp_cell_y + 2*y_sign
+                # sauf erreur, si on met tout en valeur absolue on obtient toujours une pente descendante
+                # il faut donc juste regarder si le y du point est inférieur ou supérieur au 
+                # y correspondant au x sur l'equation de droite
+
+            else : 
+                "on est dans hd ou d (ou equivalent)"
+                # On trouve l'équation de la ligne de démarcation
+                x_depart = 2.5 * radius
+                y_depart = mini_radius
+
+                x_fin = 2* radius
+                y_fin = 0
+
+                slope = (y_fin - y_depart) / (x_fin - x_depart)
+                offset = y_depart - (slope * x_depart)
+
+                y_ref = abs(pos_x) * slope + offset
+
+                # sauf erreur, si on met tout en valeur absolue on obtient toujours une pente ascendante
+                # il faut donc juste regarder si le y du point est inférieur ou supérieur au 
+                # y correspondant au x sur l'equation de droite
+                if abs(pos_y) >= abs(y_ref) :
+                   "on est dans hd"
+                   return self.find_coordinates_corner(tmp_cell_x,tmp_cell_y, x_sign,y_sign)
+                else :
+                   "on est dans d"
+                   return tmp_cell_x+ x_sign, tmp_cell_y + y_sign
+            
+        if abs(pos_x)> radius and abs(pos_x)< 2*radius :
+            "on est dans hd"
+            return self.find_coordinates_corner(tmp_cell_x,tmp_cell_y, x_sign,y_sign)
+        if abs(pos_x) > radius/2 and abs(pos_x)<= 2*radius:
+            # on est dans c, h ou hd
+            x1 = radius
+            y1 = 0
+
+            x2 = radius/2
+            y2 = mini_radius
+
+            x3 = radius
+            y3= 2*mini_radius
+
+            slope1 = (y1 - y2) / (x1 - x2)
+            offset1 = y2 - (slope1 * x2)
+            y_ref1 = slope1 * pos_x + offset1
+
+            slope2 = (y3 - y2) / (x3 - x2)
+            offset2 = y2 - (slope2 * x2)
+            y_ref2 = slope2 * pos_x + offset2
+
+
+            if abs(pos_y) >= y_ref1 and abs(pos_y) <= y_ref2:
+                # on est dans hd
+                return self.find_coordinates_corner(tmp_cell_x,tmp_cell_y, x_sign,y_sign)
+        if abs(pos_y)>mini_radius :
+            return tmp_cell_x, tmp_cell_y + y_sign*2
+        else :
+            return tmp_cell_x, tmp_cell_y
+
+
+
+    def find_coordinates_corner(self,cell_x,cell_y,x_sign,y_sign):
+        """aaaaaaaaa"""
+        f_x, f_y =0,0
+        y_even =cell_y % 2 == 0
+        if(y_even):
+            if(x_sign > 0):
+                f_x = cell_x
+            else :
+                f_x = cell_x - 1
+
+        else :
+            if(x_sign > 0):
+                f_x = cell_x+1
+            else :
+                f_x = cell_x
+
+        if(y_sign > 0):
+            f_y = cell_y +1 
+        else :
+            f_y = cell_y - 1
+
+        return f_x,f_y
+        
+
 
     def move(self, rotation, move_x, move_y):
         """Handle movement of the robot in the hexamemory"""
