d890cd494e96423d90d4928a9a313b05d5e2baa0
KNOCKAERT Titouan <tknockaert@univ-catholyon.fr>
2022-04-21 09:49:55 +0200

    oups

---
+++ stage_titouan/osoyoo_car_enacter/arduino_secrets.h
@@ -0,0 +1,12 @@
+
+////////* EXTERNOTO WIFI ///////*/
+/* #define SECRET_WIFI_TYPE "STA"*/
+//#define SECRET_SSID "RobotBSN"
+//#define SECRET_PASS "BSNgoodlife" 
+
+#define SECRET_WIFI_TYPE "STA"
+#define SECRET_SSID "Margit"
+#define SECRET_PASS "goodlife"
+
+////////* ROBOTO WIFI ///////*/
+//#define SECRET_WIFI_TYPE "AP"
