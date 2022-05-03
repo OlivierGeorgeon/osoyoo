a767a6cbb3ea3b58324d9e8989fec73fa87908a1
KNOCKAERT Titouan <tknockaert@univ-catholyon.fr>
2022-05-03 18:12:06 +0200

    ?

--- stage_titouan/Workspace.py
+++ stage_titouan/Workspace.py
@@ -22,6 +22,7 @@ class Workspace :
         self.f_ready_for_hex_refresh = False
         self.f_inde_cell_projected = False
         
+        self.f_reset_flag = False
 
         self.agent_action = None
         self.f_agent_action_ready = False
@@ -40,3 +41,34 @@ class Workspace :
 
     def action_reset(self):
         print("A FAIRE") #TODO
+        self.synthesizer.reset()
+        self.memory.reset()
+        self.hexa_memory.reset()
+        self.f_reset_flag = True
+        self.user_action = None
+        #FLAGS :
+        self.need_user_action = False
+        self.enact_step = 0
+        self.need_traitement_flag = False
+        self.f_user_action_ready = False
+        self.f_hexmem_changed = False
+        self.f_memory_changed = False
+        self.f_ready_for_hex_refresh = False
+        self.f_inde_cell_projected = False
+        
+        self.f_reset_flag = False
+
+        self.agent_action = None
+        self.f_agent_action_ready = False
+        self.action_angle = None
+
+        self.CONTROL_MODE_AUTOMATIC = "auto"
+        self.CONTROL_MODE_MANUAL = "manual"
+        self.control_mode =  self.CONTROL_MODE_MANUAL
+
+
+        self.enacted_interaction = {}
+        self.f_enacted_interaction_has_changed = False
+        self.f_new_things_in_memory = False
+
+        self.f_ready_for_next_loop = True
