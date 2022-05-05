5d4fffb20c2990e04833984532683da300d36ac4
KNOCKAERT Titouan <tknockaert@univ-catholyon.fr>
2022-05-05 17:02:06 +0200

    continue context synth

--- stage_titouan/Synthesizer/SynthesizerUserContext.py
+++ stage_titouan/Synthesizer/SynthesizerUserContext.py
@@ -1,4 +1,4 @@
-from ..  Synthesizer.SynthesizerUserInteraction import SynthesizerUserInteraction
+from ..  Synthesizer.SynthesizerUserInteraction import SynthesizerUserInteraction,math
 class SynthesizerUserContext(SynthesizerUserInteraction):
     """Synthesizer that have two modes : 
     Manual and Automatic.
@@ -35,6 +35,7 @@ class SynthesizerUserContext(SynthesizerUserInteraction):
         self.AUTOMATIC_MODE = "automatic"
         self.MANUAL_MODE = "manual"
         self.current_mode = self.AUTOMATIC_MODE
+        self.known_obstacles = []
 
     def set_mode(self, mode):
         """AUTOMATIC : synthesizer use the known context
@@ -62,6 +63,43 @@ class SynthesizerUserContext(SynthesizerUserInteraction):
     
     def context_analysis(self, real_echos):
         """Compare the positions of the real echos with the positions of the known obstacle"""
-    def project_interactions_on_internal_hexagrid(self):
 
+
+        # First step : associate each echo with the closest known obstacle
+        known_obstacles = self.known_obstacles
+        dict_association = {}
+        allocentric_coordinates = []
+        rota_radian = math.radians(self.hexa_memory.robot_angle)
+        for _,interaction in enumerate(real_echos):
+                corner_x,corner_y = interaction.x,interaction.y
+                x_prime = int(corner_x* math.cos(rota_radian) - corner_y * math.sin(rota_radian) + self.hexa_memory.robot_pos_x)
+                y_prime = int(corner_y * math.cos(rota_radian) + corner_x* math.sin(rota_radian) + self.hexa_memory.robot_pos_y)
+                allocentric_coordinates.append(((x_prime,y_prime),interaction))
+        used_obstacles = []
+        for elem in allocentric_coordinates:
+            min = None
+            min_obstacle = None
+            for obstacle in known_obstacles:
+                # compute distance between obstacle and elem
+                # keep the minimum in min
+                distance = math.sqrt((elem[0][0]-obstacle[0])**2 + (elem[0][1]-obstacle[1])**2)
+                #if bancal faut ameliorer
+                if min is None or distance < min and ([item for item in used_obstacles if item[0] == obstacle] == [] or [item for item in used_obstacles if item[0] == obstacle and item[1] < distance] != []):
+                    min = distance
+                    min_obstacle = obstacle
+                used_obstacles.append(min_obstacle,min)
+                dict_association[elem] = min_obstacle,min
+                
+        # Second step : Look if there is a missing obstacle (known obstacle that should have been detected)
+        # If there is, command the robot to scan in the direction of the obstacle
+        # if we find it : nice
+        # if we don't find it : we need to change the associations TODO
+        a = 1
+
+        # Third_step : Look for the position of the robot that match the best
+        # the distance computed in dict_association
+        # and move the robot to this position in the memory
+    def project_interactions_on_internal_hexagrid(self):
+        """blabla"""
+        a = 1
         
