c177878b8a1c583b2d4fc7454e5c76c00d3f3de8
KNOCKAERT Titouan <tknockaert@univ-catholyon.fr>
2022-05-23 16:28:51 +0200

    remove depr

--- deprecateds/stage_titouan/Model/__init__.py
+++
@@ -1,7 +0,0 @@
-# import sys
-# sys.path.insert(1, 'Model')
-# from . Agents import *
-# from . Hexamemories import *
-# from . Memories import *
-# from . Synthesizers import *
-# from .Interactions import *
