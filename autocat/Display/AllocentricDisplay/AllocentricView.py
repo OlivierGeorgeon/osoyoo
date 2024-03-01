522033c537cd1a6fd2c9f05ffc14ac1681a66b4a
assi.karim <karimassi02@gmail.com>
2024-03-01 11:46:26 +0100

    feat : -drag effect now works on Allocentric Window. Changes to AllocentricView.py

--- autocat/Display/AllocentricDisplay/AllocentricView.py
+++ autocat/Display/AllocentricDisplay/AllocentricView.py
@@ -15,6 +15,7 @@ CELL_RADIUS = 50
 ZOOM_IN_FACTOR = 1.2
 ZOOM_OUT_FACTOR = 1/ZOOM_IN_FACTOR
 
+
 class AllocentricView(InteractiveDisplay):
     """Create the allocentric view"""
     def __init__(self, workspace, width=400, height=400, *args, **kwargs):
@@ -37,8 +38,8 @@ class AllocentricView(InteractiveDisplay):
         self.robot_batch = pyglet.graphics.Batch()
         self.robot = OsoyooCar(self.robot_batch, self.forefront)  # Rectangles seem not to respect ordered groups
 
-        # self.zoom_level = 4
-        # self.mouse_press_angle = 0
+        self.zoom_level = 1
+        #self.mouse_press_angle = 0
         # self.window = None
 
         self.workspace = workspace
@@ -50,8 +51,8 @@ class AllocentricView(InteractiveDisplay):
         self.robot_poi = None  # The other robot point of interest
         self.body_poi = None
 
-        # self.mouse_press_x = 0
-        # self.mouse_press_y = 0
+        #self.mouse_press_x = 0
+        #self.mouse_press_y = 0
 
         # The text at the bottom left
         self.label_batch = pyglet.graphics.Batch()
@@ -61,19 +62,16 @@ class AllocentricView(InteractiveDisplay):
         self.label_click = pyglet.text.Label('', font_name='Verdana', font_size=10, x=10, y=30)
         self.label_click.color = (255, 255, 255, 255)
         self.label_click.batch = self.label_batch
-
-        # Initialize camera values
         self.left = 0
         self.right = width
         self.bottom = 0
         self.top = height
-        self.zoom_level = 1
         self.zoomed_width = width
         self.zoomed_height = height
 
     def init_gl(self, width, height):
         # Set clear color
-        glClearColor(0 / 255, 0 / 255, 0 / 255, 0 / 255)
+        #glClearColor(0 / 255, 0 / 255, 0 / 255, 0 / 255)
 
         # Set antialiasing
         glEnable(GL_LINE_SMOOTH)
@@ -139,12 +137,31 @@ class AllocentricView(InteractiveDisplay):
 
     def on_draw(self):
         """ Drawing the window """
-        glClear(GL_COLOR_BUFFER_BIT)
-        # The transformations are stacked, and applied backward to the vertices
+
+        # Initialize Projection matrix
+        glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
+
+        # Initialize Modelview matrix
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        # Save the default modelview matrix
+        glPushMatrix()
+
+        # Clear window with ClearColor
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        # Set orthographic projection matrix
+        glOrtho(self.left, self.right, self.bottom, self.top, 1, -1)
+        # glClear(GL_COLOR_BUFFER_BIT)
+        #
+        # # The transformations are stacked, and applied backward to the vertices
+        # glLoadIdentity()
+
         # Stack the projection matrix. Centered on (0,0). Fit the window size and zoom factor
         # glOrtho(-self.width * self.zoom_level, self.width * self.zoom_level, -self.height * self.zoom_level,
         #         self.height * self.zoom_level, 1, -1)
+
         # Draw the hexagonal grid
         self.batch.draw()
 
@@ -161,37 +178,6 @@ class AllocentricView(InteractiveDisplay):
         glOrtho(0, self.width, 0, self.height, -1, 1)
         self.label_batch.draw()
 
-        # Initialize Projection matrix
-        glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-
-        # Initialize Modelview matrix
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        # Save the default modelview matrix
-        glPushMatrix()
-
-        # Clear window with ClearColor
-
-        # Set orthographic projection matrix
-        glOrtho(self.left, self.right, self.bottom, self.top, 1, -1)
-
-        # Draw quad
-        glBegin(GL_QUADS)
-        glColor3ub(0xFF, 0, 0)
-        glVertex2i(10, 10)
-
-        glColor3ub(0xFF, 0xFF, 0)
-        glVertex2i(110, 10)
-
-        glColor3ub(0, 0xFF, 0)
-        glVertex2i(110, 110)
-
-        glColor3ub(0, 0, 0xFF)
-        glVertex2i(10, 110)
-        glEnd()
-
-        # Remove default modelview matrix
         glPopMatrix()
 
     def on_mouse_motion(self, x, y, dx, dy):
