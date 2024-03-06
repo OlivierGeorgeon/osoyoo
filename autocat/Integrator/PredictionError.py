75e4ed16c81825be3e1efb0b075d894eec9eaeca
Olivier Georgeon <olivier.georgeon@gmail.com>
2024-03-06 21:12:26 +0100

    prediction errors

--- autocat/Integrator/PredictionError.py
+++ autocat/Integrator/PredictionError.py
@@ -4,11 +4,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import os
 from pyrr import Quaternion
-from ..Decider.Action import ACTION_FORWARD
-from ..Utils import short_angle, point_to_head_direction_distance
+from ..Decider.Action import ACTION_FORWARD, ACTION_BACKWARD
+from ..Decider.Interaction import OUTCOME_LOST_FOCUS
+from ..Utils import short_angle, point_to_head_direction_distance, assert_almost_equal_angles
 from ..Integrator.OutcomeCode import CONFIDENCE_CONFIRMED_FOCUS
 
 PREDICTION_ERROR_WINDOW = 100
+RUNNING_AVERAGE_COEF = 0.25
 
 
 def plot(data_dict, caption, file_name):
@@ -44,14 +46,20 @@ class PredictionError:
     def __init__(self, workspace):
         """Initialize the prediction error arrays"""
         self.workspace = workspace
-        self.outcome_code = {}
-        self.forward_duration1 = {}  # (s)
-        self.yaw = {}  # (degree)
-        self.compass = {}  # (degree)
-        self.echo_direction = {}  # (degree)
-        self.echo_distance = {}  # (mm)
-        self.focus_direction = {}  # (degree)
-        self.focus_distance = {}  # (mm)
+        self.pe_outcome_code = {}
+        self.pe_forward_duration1 = {}  # (s)
+        self.pe_yaw = {}  # (degree)
+        self.pe_compass = {}  # (degree)
+        self.pe_echo_direction = {}  # (degree)
+        self.pe_echo_distance = {}  # (mm)
+        self.pe_focus_direction = {}  # (degree)
+        self.pe_focus_distance = {}  # (mm)
+        self.pe_speed_forward = {}  # (mm/s)
+        self.value_speed_forward = {}  # (mm/s)
+        self.pe_speed_backward = {}  # (mm/s)
+        self.value_speed_backward = {}  # (mm/s)
+
+        self.previous_echo_distance = 0
 
     def log(self, enaction):
         """Compute the prediction errors: computed - actual"""
@@ -60,77 +68,113 @@ class PredictionError:
 
         # Outcome code
         pe = int(enaction.predicted_outcome_code != enaction.outcome_code)
-        self.outcome_code[enaction.clock] = pe
-        self.outcome_code.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+        self.pe_outcome_code[enaction.clock] = pe
+        self.pe_outcome_code.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
         print("Prediction Error Outcome Code", pe,
               "(predicted:", enaction.predicted_outcome_code, ", actual:", enaction.outcome_code, ")",
-              "Average:", round(float(np.mean(list(self.outcome_code.values())))),
-              "std:", round(float(np.std(list(self.outcome_code.values())))))
+              "Average:", round(float(np.mean(list(self.pe_outcome_code.values())))),
+              "std:", round(float(np.std(list(self.pe_outcome_code.values())))))
 
         # Translation FORWARD duration1
 
         if enaction.action.action_code in [ACTION_FORWARD] and actual_outcome.duration1 != 0:
             pe = (computed_outcome.duration1 - actual_outcome.duration1) / 1000  # / actual_outcome.duration1
-            self.forward_duration1[enaction.clock] = pe
-            self.forward_duration1.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+            self.pe_forward_duration1[enaction.clock] = pe
+            self.pe_forward_duration1.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
             print("Prediction Error Translate duration1 (simulation - measured)=", round(pe),
-                  "Average:", round(float(np.mean(list(self.forward_duration1.values()))), 2),
-                  "std:", round(float(np.std(list(self.forward_duration1.values())))), 2)
+                  "Average:", round(float(np.mean(list(self.pe_forward_duration1.values()))), 2),
+                  "std:", round(float(np.std(list(self.pe_forward_duration1.values())))), 2)
+
+            # Forward speed
+
+            self.value_speed_forward[enaction.clock] = self.workspace.actions[ACTION_FORWARD].translation_speed[0]
+            if assert_almost_equal_angles(math.radians(actual_outcome.head_angle), 0, 11) and enaction.outcome_code != OUTCOME_LOST_FOCUS:
+                action_speed = self.workspace.actions[ACTION_FORWARD].translation_speed[0]
+                speed = (self.previous_echo_distance - actual_outcome.echo_distance) * 1000 / actual_outcome.duration1
+                pe = round(action_speed - speed)
+                self.pe_speed_forward[enaction.clock] = pe
+                self.pe_speed_forward.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+                print("Prediction Error Speed (simulation", round(action_speed), " - measured", round(speed), ")=", pe,
+                      "Average:", round(float(np.mean(list(self.pe_speed_forward.values()))), 1),
+                      "std:", round(float(np.std(list(self.pe_speed_forward.values())))), 1)
+                # Update the speed of the action
+                self.workspace.actions[ACTION_FORWARD].translation_speed[0] = action_speed * (1. - RUNNING_AVERAGE_COEF) + speed * RUNNING_AVERAGE_COEF
+
+        # self.previous_echo_distance = actual_outcome.echo_distance
+
+        # Translation Backward
+
+        if enaction.action.action_code in [ACTION_BACKWARD] and actual_outcome.duration1 != 0 and enaction.outcome_code != OUTCOME_LOST_FOCUS:
+            self.value_speed_backward[enaction.clock] = self.workspace.actions[ACTION_BACKWARD].translation_speed[0]
+            if assert_almost_equal_angles(math.radians(actual_outcome.head_angle), 0, 11):
+                action_speed = self.workspace.actions[ACTION_BACKWARD].translation_speed[0]
+                speed = (self.previous_echo_distance - actual_outcome.echo_distance) * 1000 / actual_outcome.duration1
+                pe = round(action_speed - speed)
+                self.pe_speed_backward[enaction.clock] = pe
+                self.pe_speed_backward.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+                print("Prediction Error Speed Back (simulation", round(action_speed), " - measured", round(speed), ")=", pe,
+                      "Average:", round(float(np.mean(list(self.pe_speed_backward.values()))), 1),
+                      "std:", round(float(np.std(list(self.pe_speed_backward.values())))), 1)
+                # Update the speed of the action
+                self.workspace.actions[ACTION_BACKWARD].translation_speed[0] = action_speed * (1. - RUNNING_AVERAGE_COEF) + speed * RUNNING_AVERAGE_COEF
+
+        self.previous_echo_distance = actual_outcome.echo_distance
+
         # yaw
 
         # pe = math.degrees(-short_angle(enaction.command.intended_yaw_quaternion, enaction.yaw_quaternion))
         pe = math.degrees(-short_angle(Quaternion.from_z_rotation(math.radians(computed_outcome.yaw)),
                                        enaction.trajectory.yaw_quaternion))
-        self.yaw[enaction.clock] = pe
-        self.yaw.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
+        self.pe_yaw[enaction.clock] = pe
+        self.pe_yaw.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
         print("Prediction Error Yaw (command - measure)=", round(pe, 1),
-              "Average:", round(float(np.mean(list(self.yaw.values()))), 1),
-              "std:", round(float(np.std(list(self.yaw.values()))), 1))
+              "Average:", round(float(np.mean(list(self.pe_yaw.values()))), 1),
+              "std:", round(float(np.std(list(self.pe_yaw.values()))), 1))
 
         # Compass prediction error
 
-        self.compass[enaction.clock] = math.degrees(enaction.trajectory.body_direction_delta)
-        self.compass.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+        self.pe_compass[enaction.clock] = math.degrees(enaction.trajectory.body_direction_delta)
+        self.pe_compass.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
         print("Prediction Error Compass (integrated direction - compass measure)=",
-              round(self.compass[enaction.clock], 2), "Average:",
-              round(float(np.mean(list(self.compass.values()))), 2), "std:",
-              round(float(np.std(list(self.compass.values()))), 2))
+              round(self.pe_compass[enaction.clock], 2), "Average:",
+              round(float(np.mean(list(self.pe_compass.values()))), 2), "std:",
+              round(float(np.std(list(self.pe_compass.values()))), 2))
 
         # The echo prediction error when focus is confident
 
         # if enaction.trajectory.focus_confidence >= CONFIDENCE_CONFIRMED_FOCUS:
         pe = computed_outcome.head_angle - actual_outcome.head_angle
-        self.echo_direction[actual_outcome.clock] = pe
-        self.echo_direction.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+        self.pe_echo_direction[actual_outcome.clock] = pe
+        self.pe_echo_direction.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
         print("Prediction Error Head Direction (prediction - measure)=", pe,
               # enaction.trajectory.focus_direction_prediction_error,
-              "Average:", round(float(np.mean(list(self.echo_direction.values())))),
-              "std:", round(float(np.std(list(self.echo_direction.values())))))
+              "Average:", round(float(np.mean(list(self.pe_echo_direction.values())))),
+              "std:", round(float(np.std(list(self.pe_echo_direction.values())))))
 
         if computed_outcome.echo_distance < 10000 and actual_outcome.echo_distance < 10000:
             pe = computed_outcome.echo_distance - actual_outcome.echo_distance
-            self.echo_distance[enaction.clock] = pe
-            self.echo_distance.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
+            self.pe_echo_distance[enaction.clock] = pe
+            self.pe_echo_distance.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
             print("Prediction Error Echo Distance (prediction - measure)=", pe,
                   # enaction.trajectory.focus_distance_prediction_error,
-                  "Average:", round(float(np.mean(list(self.echo_distance.values())))),
-                  "std:", round(float(np.std(list(self.echo_distance.values())))))
+                  "Average:", round(float(np.mean(list(self.pe_echo_distance.values())))),
+                  "std:", round(float(np.std(list(self.pe_echo_distance.values())))))
 
         # The focus prediction error
 
         if enaction.predicted_memory.egocentric_memory.focus_point is not None and enaction.trajectory.focus_point is not None:
             pa, pd = point_to_head_direction_distance(enaction.predicted_memory.egocentric_memory.focus_point)
             ma, md = point_to_head_direction_distance(enaction.trajectory.focus_point)
-            self.focus_direction[actual_outcome.clock] = round(pa - ma)
-            self.focus_direction.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
+            self.pe_focus_direction[actual_outcome.clock] = round(pa - ma)
+            self.pe_focus_direction.pop(actual_outcome.clock - PREDICTION_ERROR_WINDOW, None)
             print("Prediction Error Focus Direction (prediction - measure)=", round(pa - ma),
-                  "Average:", round(float(np.mean(list(self.focus_direction.values())))),
-                  "std:", round(float(np.std(list(self.focus_direction.values())))))
-            self.focus_distance[enaction.clock] = round(pd - md)
-            self.focus_distance.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
+                  "Average:", round(float(np.mean(list(self.pe_focus_direction.values())))),
+                  "std:", round(float(np.std(list(self.pe_focus_direction.values())))))
+            self.pe_focus_distance[enaction.clock] = round(pd - md)
+            self.pe_focus_distance.pop(enaction.clock - PREDICTION_ERROR_WINDOW, None)
             print("Prediction Error Focus Distance (prediction - measure)=", round(pd - md),
-                  "Average:", round(float(np.mean(list(self.focus_distance.values())))),
-                  "std:", round(float(np.std(list(self.focus_distance.values())))))
+                  "Average:", round(float(np.mean(list(self.pe_focus_distance.values())))),
+                  "std:", round(float(np.std(list(self.pe_focus_distance.values())))))
 
         # The terrain origin prediction error
 
@@ -152,14 +196,18 @@ class PredictionError:
         if not os.path.exists("log"):
             os.makedirs("log")
         # Generate the plots
-        plot(self.outcome_code, "Outcome code (0/1)", "Outcome_code")
-        plot(self.forward_duration1, "Forward duration (s)", "Forward_duration")
-        plot(self.yaw, "Yaw (degrees)", "yaw")
-        plot(self.compass, "Compass (degree)", "Compass")
-        plot(self.echo_direction, "Head direction (degree)", "Head_direction")
-        plot(self.echo_distance, "Echo distance (mm)", "Echo_distance")
-        plot(self.focus_direction, "Focus direction (degree)", "Focus_direction")
-        plot(self.focus_distance, "Focus distance (mm)", "Focus_distance")
+        plot(self.pe_outcome_code, "Outcome code (0/1)", "Outcome_code")
+        plot(self.pe_forward_duration1, "Forward duration (s)", "Forward_duration")
+        plot(self.pe_speed_forward, "Forward speed (mm/s)", "Forward_speed_pe")
+        plot(self.value_speed_forward, "Forward speed value (mm/s)", "Forward_speed_value")
+        plot(self.pe_speed_backward, "Backward speed (mm/s)", "Backward_speed_pe")
+        plot(self.value_speed_backward, "Backward speed value (mm/s)", "Backward_speed_value")
+        plot(self.pe_yaw, "Yaw (degrees)", "yaw")
+        plot(self.pe_compass, "Compass (degree)", "Compass")
+        plot(self.pe_echo_direction, "Head direction (degree)", "Head_direction")
+        plot(self.pe_echo_distance, "Echo distance (mm)", "Echo_distance")
+        plot(self.pe_focus_direction, "Focus direction (degree)", "Focus_direction")
+        plot(self.pe_focus_distance, "Focus distance (mm)", "Focus_distance")
         terrain = self.workspace.memory.phenomenon_memory.terrain()
         if terrain is not None:
             plot(terrain.origin_prediction_error, "Terrain origin (mm)", "Origin")
